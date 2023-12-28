d4523a20b39929414e8f8d862951f77a7668179e
Royweru <royezi17@gmail.com>
2023-12-28 23:19:51 +0300

    modified:   components/modal/create-listing.tsx 	new file:   components/ui/form.tsx 	new file:   components/ui/label.tsx 	modified:   package-lock.json 	modified:   package.json

--- components/modal/create-listing.tsx
+++ components/modal/create-listing.tsx
@@ -10,20 +10,33 @@ import {
 } from "../ui/dialog";
 import useCreateListingModal from "@/hooks/use-create-listing";
 
+enum STEPS{
+    LOCATION = 1,
+    CATEGORY=2,
+    DETAILS=3,
+    PRICE=4
+}
+
 const CreateListing = () => {
   const { isOpen, onClose } = useCreateListingModal();
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className=" bg-gray-200 p-5 w-full overflow-hidden">
         <DialogHeader className=" pt-4 ">
-          <DialogTitle>Welcome user, let us create our listing</DialogTitle>
+          <DialogTitle className=" font-bold font-sans text-xl">
+            Welcome user, let us create our listing
+         </DialogTitle>
           <DialogDescription>
             Let us create our first listing and let us go through the steps Let
             us create our first listing and let us go through the steps Let us
             create our first listing and let us go through the steps
           </DialogDescription>
         </DialogHeader>
-        
+        <div className=" w-full flex flex-col ">
+            <form>
+
+            </form>
+        </div>
         <DialogFooter>
            
         </DialogFooter>
