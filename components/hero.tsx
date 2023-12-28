fcaf53a6d075cbbb1a6ec1cb20d402acae64fb62
Royweru <royezi17@gmail.com>
2023-12-28 09:03:39 +0300

    modified:   app/layout.tsx 	modified:   components/hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,7 +8,7 @@ export const Hero:React.FC<HeroProps> = ({
     children
 }) => {
   return (
-    <div className=" w-full h-[500px] bg-url[('/images/bg3.webp')] bg-cover bg-center relative p-2">
+    <div className=" w-full h-[500px] bg-[url('/images/bg3.webp')] bg-cover bg-center relative p-0">
         {children}
     </div>
   )
